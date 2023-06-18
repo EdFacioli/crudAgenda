e35e5d3b5b89c617836eee67d6335344868e88cb
Edson Facioli <edfacioli@gmail.com>
2023-06-18 10:59:13 -0300

    Alteracoes propostas por Luiz, adicao de font-awesome, uso do componente confirm para exclusao

--- src/main/java/br/com/desstecnologia/desstecnologia/bean/ClienteBean.java
+++ src/main/java/br/com/desstecnologia/desstecnologia/bean/ClienteBean.java
@@ -1,6 +1,6 @@
 package br.com.desstecnologia.desstecnologia.bean;
 
-import br.com.desstecnologia.desstecnologia.Dao.ClienteDao;
+import br.com.desstecnologia.desstecnologia.dao.ClienteDao;
 import br.com.desstecnologia.desstecnologia.model.Clientes;
 
 import javax.faces.application.FacesMessage;
@@ -15,50 +15,63 @@ import java.util.HashMap;
 public class ClienteBean implements Serializable {
 
     ClienteDao clienteDao = ClienteDao.getInstance();
-    private HashMap<Integer, Clientes> clientesList;
+    private HashMap<Integer, Clientes> clientesStore;
     private Clientes cliente;
     private Integer id;
 
     public void salvar() {
-        clienteDao.save(cliente);
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Salvo com sucesso!"));
-        clientesList = clienteDao.findAll();
+        if (clienteDao.save(cliente) != null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Salvo com sucesso!"));
+            clientesStore = clienteDao.findAll();
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Não pode ser salvo!"));
+        }
         limpar();
     }
 
     public void excluir(Integer id) {
-        clienteDao.delete(id);
-        clientesList = clienteDao.findAll();
+        if (clienteDao.delete(id) != null) {
+            clientesStore = clienteDao.findAll();
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Não pode ser excluído!"));
+        }
     }
 
     public void atualizar(Integer id) {
-        clienteDao.merge(id, cliente);
-        clientesList = clienteDao.findAll();
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Atualizado com sucesso!"));
+        if (clienteDao.update(id, cliente) != null) {
+            clientesStore = clienteDao.findAll();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Atualizado com sucesso!"));
+            limpar();
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Não pode ser atualizado!"));
+        }
     }
 
     public void selecionado(Integer id) {
         this.id = id;
         this.cliente = clienteDao.findById(id);
+        if (this.cliente == null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Não existe o registro!"));
+        }
     }
 
     public void carregarDados() {
-        clientesList = clienteDao.findAll();
+        clientesStore = clienteDao.findAll();
     }
 
     public void limpar() {
         cliente = null;
     }
 
-    public HashMap<Integer, Clientes> getClientesList() {
-        if (clientesList == null) {
-            clientesList = new HashMap<>();
+    public HashMap<Integer, Clientes> getClientesStore() {
+        if (clientesStore == null) {
+            clientesStore = new HashMap<>();
         }
-        return clientesList;
+        return clientesStore;
     }
 
-    public void setClientesList(HashMap<Integer, Clientes> clientesList) {
-        this.clientesList = clientesList;
+    public void setClientesStore(HashMap<Integer, Clientes> clientesStore) {
+        this.clientesStore = clientesStore;
     }
 
     public Clientes getCliente() {
